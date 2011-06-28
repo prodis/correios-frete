f11d01c15eb77d7f7063d76ee696c14e293e6ad0
Prodis <prodis@gmail.com>
2011-06-28 18:15:04 -0300

    Frete.new with options.

--- lib/correios/frete.rb
+++ lib/correios/frete.rb
@@ -6,16 +6,16 @@ module Correios
     attr_accessor :peso, :comprimento, :altura, :largura, :diametro
     attr_accessor :formato, :mao_propria, :aviso_recebimento, :valor_declarado
 
-    ATRIBUTOS_PADRAO = {
+    DEFAULT_OPTIONS = {
       :formato => :caixa_pacote, 
       :mao_propria => false, 
       :aviso_recebimento => false, 
-      :valor_declarado => 0
+      :valor_declarado => 0.0
     }
 
-    def initialize(atributos = {})
-      ATRIBUTOS_PADRAO.merge(atributos).each do |atributo, valor|
-        self.send("#{atributo}=", valor)
+    def initialize(options = {})
+      DEFAULT_OPTIONS.merge(options).each do |attr, value|
+        self.send("#{attr}=", value)
       end
 
       yield self if block_given?
--- spec/correios/frete_spec.rb
+++ spec/correios/frete_spec.rb
@@ -3,23 +3,43 @@ require 'spec_helper'
 
 describe Correios::Frete do
   describe ".new" do
-    context "cria com valor padrão de" do
+    context "create with default value of" do
       before(:each) { @frete = Correios::Frete.new }
 
       it "formato" do
         @frete.formato.should == :caixa_pacote
       end
 
-      it "mão própria" do
+      it "mao_propria" do
         @frete.mao_propria.should be_false
       end
 
-      it "aviso de recebimento" do
+      it "aviso_recebimento" do
         @frete.aviso_recebimento.should be_false
       end
 
-      it "valor declarado" do
+      it "valor_declarado" do
         @frete.valor_declarado.should == 0
+      end
+    end
+
+    { :cep_origem => "01000-000", 
+      :cep_destino => "021222-222",
+      :peso => 0.321, 
+      :comprimento => 12.5, 
+      :altura => 1.4, 
+      :largura => 4.6, 
+      :diametro => 5.0,
+      :formato => :rolo_prisma, 
+      :mao_propria => true, 
+      :aviso_recebimento => true, 
+      :valor_declarado => 1.99
+    }.each do |attr, value|
+      context "when #{attr} is supplied" do
+        it "set #{attr} value" do
+          @frete = Correios::Frete.new(attr => value)
+          @frete.send(attr).should == value
+        end
       end
     end
   end
