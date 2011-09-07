9e9dc6e13069da091e99b4cef4857801a254a5da
Prodis <prodis@gmail.com>
2011-09-07 00:35:24 -0300

    Bump to version 1.1.0.

--- lib/correios/frete/version.rb
+++ lib/correios/frete/version.rb
@@ -3,8 +3,8 @@ module Correios
   module Frete
     module Version
       MAJOR = 1
-      MINOR = 0
-      PATCH = 2
+      MINOR = 1
+      PATCH = 0
       VERSION = [MAJOR, MINOR, PATCH].join(".")
     end
   end
