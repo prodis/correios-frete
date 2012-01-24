12c5172f191b6ee0c1f312ab240e350482499818
Prodis <prodis@gmail.com>
2012-01-24 16:35:38 -0200

    Remove Barbolo's package classes.

--- lib/correios-frete.rb
+++ lib/correios-frete.rb
@@ -2,8 +2,6 @@
 require 'rubygems'
 require 'correios/frete'
 require 'correios/frete/calculador'
-require 'correios/frete/calculador_multiplo'
-require 'correios/frete/item'
 require 'correios/frete/pacote'
 require 'correios/frete/pacote_item'
 require 'correios/frete/parser'
