b7944f05adf1668196f4ae1900a719f1f1ee0903
Prodis <prodis@gmail.com>
2012-08-15 12:19:25 -0300

    Tests running in random order.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,13 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'correios-frete'
-require 'rspec'
 require 'webmock/rspec'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.order = "random"
 end
 
 WebMock.disable_net_connect!
