58972ab143fe0eb316513f0eb65630bb24b0ae22
Paulo Henrique Sacramento <contato@henriquesacramento.net>
2017-05-09 15:29:37 -0300

    Substituicao dos códigos do PAC e do SEDEX (#31)
    * Changed Version and Codes for new Codes from Correios
    
    * Updated Specs
    
    * Updated Readme to new examples

--- lib/correios/frete/servico.rb
+++ lib/correios/frete/servico.rb
@@ -7,10 +7,10 @@ module Correios
       include SAXMachine
 
       AVAILABLE_SERVICES = {
-        "41106" => { :type => :pac,                         :name => "PAC",            :description => "PAC sem contrato"                  },
+        "04510" => { :type => :pac,                         :name => "PAC",            :description => "PAC sem contrato"                  },
         "41068" => { :type => :pac_com_contrato,            :name => "PAC",            :description => "PAC com contrato"                  },
         "41300" => { :type => :pac_gf,                      :name => "PAC GF",         :description => "PAC para grandes formatos"         },
-        "40010" => { :type => :sedex,                       :name => "SEDEX",          :description => "SEDEX sem contrato"                },
+        "04014" => { :type => :sedex,                       :name => "SEDEX",          :description => "SEDEX sem contrato"                },
         "40045" => { :type => :sedex_a_cobrar,              :name => "SEDEX a Cobrar", :description => "SEDEX a Cobrar, sem contrato"      },
         "40126" => { :type => :sedex_a_cobrar_com_contrato, :name => "SEDEX a Cobrar", :description => "SEDEX a Cobrar, com contrato"      },
         "40215" => { :type => :sedex_10,                    :name => "SEDEX 10",       :description => "SEDEX 10, sem contrato"            },
